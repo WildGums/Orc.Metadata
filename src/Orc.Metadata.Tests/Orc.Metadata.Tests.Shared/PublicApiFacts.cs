eeb71a4f4e8e14a4e9ae4f3dad4a456ca130f492
Geert van Horrik <geert@catenalogic.com>
2017-10-01 19:20:46 +0200

    Enable ApiApprover to make sure we don't introduce unwanted breaking changes

---
+++ src/Orc.Metadata.Tests/Orc.Metadata.Tests.Shared/PublicApiFacts.cs
@@ -0,0 +1,24 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="PublicApiFacts.cs" company="WildGums">
+//   Copyright (c) 2008 - 2017 WildGums. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+
+namespace Orc.Metadata.Tests
+{
+    using ApiApprover;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class PublicApiFacts
+    {
+        [Test]
+        public void Orc_Metadata_HasNoBreakingChanges()
+        {
+            var assembly = typeof(MetadataProvider).Assembly;
+
+            PublicApiApprover.ApprovePublicApi(assembly);
+        }
+    }
+}
