b0415454996faa02ec49e403ede8ba532b432576
Geert van Horrik <geert@catenalogic.com>
2026-04-08 09:26:48 +0200

    Convert to file scoped namespaces

--- src/Orc.Metadata.Tests/Helpers/ServiceCollectionHelper.cs
+++ src/Orc.Metadata.Tests/Helpers/ServiceCollectionHelper.cs
@@ -1,20 +1,19 @@
-﻿namespace Orc.Metadata.Tests
+﻿namespace Orc.Metadata.Tests;
+
+using Catel;
+using Microsoft.Extensions.DependencyInjection;
+using Orc.Metadata;
+
+internal static class ServiceCollectionHelper
 {
-    using Catel;
-    using Microsoft.Extensions.DependencyInjection;
-    using Orc.Metadata;
-
-    internal static class ServiceCollectionHelper
+    public static IServiceCollection CreateServiceCollection()
     {
-        public static IServiceCollection CreateServiceCollection()
-        {
-            var serviceCollection = new ServiceCollection();
+        var serviceCollection = new ServiceCollection();
 
-            serviceCollection.AddLogging();
-            serviceCollection.AddCatelCore();
-            serviceCollection.AddOrcMetadata();
+        serviceCollection.AddLogging();
+        serviceCollection.AddCatelCore();
+        serviceCollection.AddOrcMetadata();
 
-            return serviceCollection;
-        }
+        return serviceCollection;
     }
 }
--- src/Orc.Metadata/OrcMetadataModule.cs
+++ src/Orc.Metadata/OrcMetadataModule.cs
@@ -1,25 +1,24 @@
-﻿namespace Orc
+﻿namespace Orc;
+
+using Catel.Services;
+using Catel.ThirdPartyNotices;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Orc.Metadata;
+
+/// <summary>
+/// Core module which allows the registration of default services in the service collection.
+/// </summary>
+public static class OrcMetadataModule
 {
-    using Catel.Services;
-    using Catel.ThirdPartyNotices;
-    using Microsoft.Extensions.DependencyInjection;
-    using Microsoft.Extensions.DependencyInjection.Extensions;
-    using Orc.Metadata;
-
-    /// <summary>
-    /// Core module which allows the registration of default services in the service collection.
-    /// </summary>
-    public static class OrcMetadataModule
+    public static IServiceCollection AddOrcMetadata(this IServiceCollection serviceCollection)
     {
-        public static IServiceCollection AddOrcMetadata(this IServiceCollection serviceCollection)
-        {
-            serviceCollection.TryAddSingleton<IMetadataProvider, MetadataProvider>();
+        serviceCollection.TryAddSingleton<IMetadataProvider, MetadataProvider>();
 
-            serviceCollection.AddSingleton<ILanguageSource>(new LanguageResourceSource("Orc.Metadata", "Orc.Metadata.Properties", "Resources"));
+        serviceCollection.AddSingleton<ILanguageSource>(new LanguageResourceSource("Orc.Metadata", "Orc.Metadata.Properties", "Resources"));
 
-            serviceCollection.AddSingleton<IThirdPartyNotice>((x) => new LibraryThirdPartyNotice("Orc.Metadata", "https://github.com/wildgums/orc.metadata"));
+        serviceCollection.AddSingleton<IThirdPartyNotice>((x) => new LibraryThirdPartyNotice("Orc.Metadata", "https://github.com/wildgums/orc.metadata"));
 
-            return serviceCollection;
-        }
+        return serviceCollection;
     }
 }
